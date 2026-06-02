adeeb82d0cb6b984dc508dad947ca2a28d5b7cd8
zLucky090802 <d.espitia15@gmail.com>
2026-06-02 15:00:04 -0500

    defining settings and imports main.py

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
+from dotenv import load_dotenv
+import os
+
+from llama_index.core import Settings
+from llama_index.llms.groq import Groq
+from llama_index.core.agent.workflow import (
+    ReActAgent,
+    AgentStream,
+    ToolCallResult,
+    FunctionAgent
+)
+
+load_dotenv()
+
+api_key_groq = os.getenv('GROQ_API_KEY')
+llm = Groq(
+    model='llama-3.1-8b-instant',
+    api_key=api_key_groq
+)
+
+Settings.llm = llm
+
+
+
 def main():
+    
+    
     print("Hello from jarvis!")
 
 
